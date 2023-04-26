f5ae88ca33e785be5b540476e6a38d1a188e1c77
Bart Groothuysen <bart@groothuysen.com>
2023-04-26 08:48:57 +0200

    database.php (php toegevoegd 1)

---
+++ site/database.php
@@ -0,0 +1,9 @@
+// Database configuratie
+<?php
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "superheroes";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
