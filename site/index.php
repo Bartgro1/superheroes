27d4d72debcf7a200e25e2c7d79e591c95739e8d
Bart Groothuysen <bart@groothuysen.com>
2023-04-26 09:11:51 +0200

    index.php tabel gemaakt

--- site/index.php
+++ site/index.php
@@ -5,14 +5,50 @@ require 'database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    // Hier komen alle superheroes
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Titel</th>
+                    <th>Alignment</th>
+                    <th>Gender</th>
+                    <th>Height</th>
+                    <th>Weigth</th> 
+                    <th>Identity</th>      
+                    <th>MarialStatus</th>     
+                    <th>Eyes</th>       
+                    <th>Hair</th>
+                    <th>PlaceOfBirth</th>
+                    <th>PlaceOfDead</th>
+                    <th>Citizenship</th>
+                    <th>Occupation</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+
 </body>
+
 </html>
-// Hier komen alle superheroes
