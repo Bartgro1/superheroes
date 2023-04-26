c2aba1ca303d4dc11edc62c1f8731ba59145b6cb
Bart Groothuysen <bart@groothuysen.com>
2023-04-26 08:54:15 +0200

    database connection getest

--- site/database.php
+++ site/database.php
@@ -1,9 +1,20 @@
 // Database configuratie
 <?php
-$host  = "localhost";
-$dbuser = "root";
-$dbpass = "";
+$host  = "mariadb";
+$dbuser = "user";
+$dbpass = "password";
 $dbname = "superheroes";
 
 // Maak een  database connectie
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+
+// Controleer de verbinding
+if(mysqli_connect_error())
+{
+ echo "Connection establishing failed!";
+}
+else
+{
+ echo "Connection established successfully.";
+}
