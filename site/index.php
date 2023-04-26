255997e0c4d88f673c9e0dc484679e0981bbb77d
Bart Groothuysen <bart@groothuysen.com>
2023-04-26 09:55:18 +0200

    laatste wijzigingen (26-4-2023)

--- site/index.php
+++ site/index.php
@@ -1,15 +1,22 @@
 <?php
 
+
+
 require 'database.php';
 
+$search = $_POST['search'];
+$sql = "SELECT * from superheroes where ID = $search";
 
-$sql = "SELECT * from superheroes";
 
 //hier wordt de query uitgevoerd met de database
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 
 
 $superheroes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -25,6 +32,10 @@ $superheroes = mysqli_fetch_all($result, MYSQLI_ASSOC);
 <body>
     // Hier komen alle superheroes
     <main>
+        <form action="index.php" method="post">
+            <input type="search" name="search" id="search">
+            <button type="submit"> </button>
+        </form>
         <table>
             <thead>
                 <tr>
@@ -33,35 +44,35 @@ $superheroes = mysqli_fetch_all($result, MYSQLI_ASSOC);
                     <th>Alignment</th>
                     <th>Gender</th>
                     <th>Height</th>
-                    <th>Weigth</th> 
-                    <th>Identity</th>      
-                    <th>MaritalStatus</th>     
-                    <th>Eyes</th>       
+                    <th>Weigth</th>
+                    <th>Identity</th>
+                    <th>MaritalStatus</th>
+                    <th>Eyes</th>
                     <th>Hair</th>
                     <th>PlaceOfBirth</th>
                     <th>Citizenship</th>
                     <th>Occupation</th>
                 </tr>
             </thead>
-       
+
             <tbody>
-                <?php foreach($superheroes as $superhero): ?>
-                <tr>
-                    <td><?php echo $superhero['ID'] ?></td>
-                    <td><?php echo $superhero['Title'] ?></td>
-                    <td><?php echo $superhero['Alignment'] ?></td>
-                    <td><?php echo $superhero['Gender'] ?></td>
-                    <td><?php echo $superhero['Height'] ?></td>
-                    <td><?php echo $superhero['Weight'] ?></td>
-                    <td><?php echo $superhero['Identity'] ?></td>
-                    <td><?php echo $superhero['MaritalStatus']?></td>
-                    <td><?php echo $superhero['Eyes']?></td>
-                    <td><?php echo $superhero['Hair']?></td>
-                    <td><?php echo $superhero['PlaceOfBirth']?></td>
-                    <td><?php echo $superhero['Citizenship']?></td>
-                    <td><?php echo $superhero['Occupation']?></td>
-                </tr>
-                <?php endforeach;?>
+                <?php foreach ($superheroes as $superhero) : ?>
+                    <tr>
+                        <td><?php echo $superhero['ID'] ?></td>
+                        <td><?php echo $superhero['Title'] ?></td>
+                        <td><?php echo $superhero['Alignment'] ?></td>
+                        <td><?php echo $superhero['Gender'] ?></td>
+                        <td><?php echo $superhero['Height'] ?></td>
+                        <td><?php echo $superhero['Weight'] ?></td>
+                        <td><?php echo $superhero['Identity'] ?></td>
+                        <td><?php echo $superhero['MaritalStatus'] ?></td>
+                        <td><?php echo $superhero['Eyes'] ?></td>
+                        <td><?php echo $superhero['Hair'] ?></td>
+                        <td><?php echo $superhero['PlaceOfBirth'] ?></td>
+                        <td><?php echo $superhero['Citizenship'] ?></td>
+                        <td><?php echo $superhero['Occupation'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </main>
